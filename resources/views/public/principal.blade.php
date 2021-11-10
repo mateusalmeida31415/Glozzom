a29b8528717f4c6f47ce621bb429312b90b02f2c
mateusalmeida31415 <mateusalmeida31415@gmail.com>
2021-11-10 20:25:25 -0300

    Finalizando a navbar

--- resources/views/public/principal.blade.php
+++ resources/views/public/principal.blade.php
@@ -4,5 +4,16 @@
 
 @section('content')
     {{-- Navbar --}}
+    <navbar-component 
+    id="main-nav"
+    :brand="[{'name': 'Glozzom', 'link': '#'}]"
+    :itens="[
+        {'text': 'home', 'link': '#', 'ref': ''},
+        {'text': 'about us', 'link': '#', 'ref': ''},
+        {'text': 'services', 'link': '#', 'ref': ''},
+        {'text': 'blog', 'link': '#', 'ref': ''},
+        {'text': 'contact', 'link': '#', 'ref': ''}
+    ]">
+    </navbar-component>
 @endsection
     

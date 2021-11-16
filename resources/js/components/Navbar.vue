b823bfb65627f350fe37bdf056007a558ddb28bb
mateusalmeida31415 <mateusalmeida31415@gmail.com>
2021-11-16 14:03:54 -0300

    Melhorando o espaço entre os botoes e a responsividade da navbar

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark" :id="id">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark" :id="id">
         <div class="container">
             <!-- Nome da marca -->
             <a class="navbar-brand" :href="brand[0].link">{{brand[0].name}}</a>

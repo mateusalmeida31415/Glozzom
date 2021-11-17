b5040093bb8daea206ad458183fd02a317ab741c
mateusalmeida31415 <mateusalmeida31415@gmail.com>
2021-11-17 10:59:42 -0300

    Finalizaçao da feedback section

--- resources/js/app.js
+++ resources/js/app.js
@@ -41,8 +41,10 @@ Vue.component('newsletter-component', require('./components/Newsletter.vue').def
 Vue.component('footer-component', require('./components/Footer.vue').default);
 // Header
 Vue.component('header-component', require('./components/Header.vue').default);
-//What we do
+// What we do
 Vue.component('what-we-do-component', require('./components/WhatWeDo.vue').default);
+// Feedback
+Vue.component('feedback-component', require('./components/Feedback.vue').default);
 
 
 //Filtros

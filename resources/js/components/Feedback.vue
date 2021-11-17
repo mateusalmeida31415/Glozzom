c04b6aaa35c786842ae0c2be93006b7c58d484dd
mateusalmeida31415 <mateusalmeida31415@gmail.com>
2021-11-17 13:49:22 -0300

    Ajuste de responsividade na section feedback

--- resources/js/components/Feedback.vue
+++ resources/js/components/Feedback.vue
@@ -2,18 +2,22 @@
         <div class="container text-center">
             <!-- Título principal -->
             <h2>{{feedback[0].title}}</h2>
-            <div class="slider">
-                <!-- Feedbacks -->
-                <div v-for="f in feedback[0].feedback">
-                    <blockquote class="blockquote text-center">
-                        <!-- Texto do feedback -->
-                        <p class="mb-0">{{f.text}}</p>
-                        <!-- Nome do escritor do feedback -->
-                        <footer class="blockquote-footer">{{f.name}}
-                            <!-- Descrição do escritor -->
-                            <cite :title="f.source">{{f.source}}</cite>
-                        </footer>
-                    </blockquote>
+            <div class="row">
+                <div class="col-10 offset-1">
+                    <div class="slider">
+                        <!-- Feedbacks -->
+                        <div v-for="f in feedback[0].feedback">
+                            <blockquote class="blockquote text-center">
+                                <!-- Texto do feedback -->
+                                <p class="mb-0">{{f.text}}</p>
+                                <!-- Nome do escritor do feedback -->
+                                <footer class="blockquote-footer">{{f.name}}
+                                    <!-- Descrição do escritor -->
+                                    <cite :title="f.source">{{f.source}}</cite>
+                                </footer>
+                            </blockquote>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
